bc438159199426c8198c9f63bd397f276e376409
Farshid Besharati <farshidbt@gmail.com>
2011-12-02 18:42:57 +0100

    2-minuters utkast

---
+++ kod/check.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/**
+ *
+ * @author Farshid Besharati
+ * @author Love Lindström
+ * @author Niklas Forsmark
+ * @author Linnea Sandelin
+ * @author Jonas Rosenlind
+ */
+public class check {
+
+    private int sarray[][] = new array[9][9]; // för testning
+
+    private boolean checker(int x, int y) {
+    }
+}
+
