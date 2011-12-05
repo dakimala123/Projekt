6ca08f51ba005756adc19021bcfefb0eb9a807d2
MrLove <lindstrom.love@gmail.com>
2011-12-05 23:18:56 +0100

    första prototyp klar :)
    Signed-off-by: MrLove <lindstrom.love@gmail.com>

--- solve/Sudoku.java
+++ solve/Sudoku.java
@@ -2,30 +2,56 @@ package solve;
 
 public class Sudoku extends Check {
 
-        private void solve(int x, int y) {
-            if (x > 8) {
-                x = 0;
-                ++y;
-                if (y > 8) {
-                    exitAndCheck();
-                } else {
-                    solve(x, y);
-                }
-            } else if (arr[x][y] != 0) {
-                solve(++x, y);
-            } else {
-                for (n = 1; n < 10; n++) {
-                    if (check(x, y, n) {
+    private int[][] sudokuArray;
+
+    public Sudoku(int[][] someArray){
+	sudokuArray = someArray;
+    }
+    public Sudoku(string sudokuString){
+	throw new UnsupportedOperationException("String constructor not yet implemented");
+    }
+    
+    private void solve(int x, int y) {
+	if (x > 8) {
+	    x = 0;
+	    ++y;
+	    if (y > 8) {
+		exitAndCheck();
+	    } else {
+		solve(x, y);
+	    }
+	} else if (arr[x][y] != 0) {
+	    solve(++x, y);
+	} else {
+	    for (n = 1; n < 10; n++) {
+		if (check(x, y, n) {
                         arr[x][y] = n;
                         solve(++x, y);
                     }
-                }
+		    }
                 arr[x][y] = 0;
             }
-            return;
+            //return; //tror inte vi behöver det här
         }
 
-    private void exitAndCheck() {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	private void exitAndCheck() {
+	    if(verify()){
+		print();
+		System.exit();
+	    } else {
+		throw new RunTimeException("Sudoku was not correctly solved");
+	    }
+
+    
+	}
+	private void print(){
+	    
+	    for(int y = 0; y < 9; y++){
+		for(int x = 0; x < 9; x++){
+		    System.out.print(""+(sudokuArray[x][y]));
+		}
+		System.out.println();
+	    }
+	}
+
     }
