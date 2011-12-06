94daa2227724866f1ed763d9235ab12e1ea3a105
Farshid Besharati <farshidbt@gmail.com>
2011-12-06 09:32:52 +0100

    Fixade div syntaxfel och indentering.

--- solve/Sudoku.java
+++ solve/Sudoku.java
@@ -4,54 +4,54 @@ public class Sudoku extends Check {
 
     private int[][] sudokuArray;
 
-    public Sudoku(int[][] someArray){
-	sudokuArray = someArray;
+    public Sudoku(int[][] someArray) {
+        sudokuArray = someArray;
     }
-    public Sudoku(string sudokuString){
-	throw new UnsupportedOperationException("String constructor not yet implemented");
+
+    public Sudoku(String sudokuString) {
+        throw new UnsupportedOperationException("String constructor not yet implemented");
     }
-    
+
     private void solve(int x, int y) {
-	if (x > 8) {
-	    x = 0;
-	    ++y;
-	    if (y > 8) {
-		exitAndCheck();
-	    } else {
-		solve(x, y);
-	    }
-	} else if (arr[x][y] != 0) {
-	    solve(++x, y);
-	} else {
-	    for (n = 1; n < 10; n++) {
-		if (check(x, y, n) {
-                        arr[x][y] = n;
-                        solve(++x, y);
-                    }
-		    }
-                arr[x][y] = 0;
+        if (x > 8) {
+            x = 0;
+            ++y;
+            if (y > 8) {
+                exitAndCheck();
+            } else {
+                solve(x, y);
             }
-            //return; //tror inte vi behöver det här
+        } else if (sudokuArray[x][y] != 0) {
+            solve(++x, y);
+        } else {
+            for (int n = 1; n < 10; n++) {
+                if (check(x, y, n)) {
+                    sudokuArray[x][y] = n;
+                    solve(++x, y);
+                }
+            }
+            sudokuArray[x][y] = 0;
+        }
+    }
+
+    private void exitAndCheck() {
+        if (verify()) {
+            print();
+            System.exit();
+        } else {
+            throw new RunTimeException("Sudoku was not correctly solved");
         }
 
-	private void exitAndCheck() {
-	    if(verify()){
-		print();
-		System.exit();
-	    } else {
-		throw new RunTimeException("Sudoku was not correctly solved");
-	    }
-
-    
-	}
-	private void print(){
-	    
-	    for(int y = 0; y < 9; y++){
-		for(int x = 0; x < 9; x++){
-		    System.out.print(""+(sudokuArray[x][y]));
-		}
-		System.out.println();
-	    }
-	}
 
     }
+
+    private void print() {
+
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
+                System.out.print("" + (sudokuArray[x][y]));
+            }
+            System.out.println();
+        }
+    }
+}
