737b7832738be1e59dffe3e6df3e13d979337183
Farshid Besharati <farshidbt@gmail.com>
2011-12-06 17:04:58 +0100

    Bygger om matrixcheck till if-baserad.

--- solve/check.java
+++ solve/check.java
@@ -1,4 +1,5 @@
 package solve;
+
 /**
  *
  * @author Farshid Besharati
@@ -46,26 +47,26 @@ public class check extends verification {
         }
         return false;
     }
-
-    private int determineX(int x) {
-        if ((0 <= x) && (x <= 2)) {
-            return 0;
-        } else if ((3 <= x) && (x <= 5)) {
-            return 60;
-        } else {
-            return 20;
-        }
-    }
-
-    private int determineY(int y) {
-        if ((0 <= y) && (y <= 2)) {
-            return 0;
-        } else if ((3 <= y) && (y <= 5)) {
-            return 25;
-        } else {
-            return 10;
-        }
-    }
+//
+//    private int determineX(int x) {
+//        if ((0 <= x) && (x <= 2)) {
+//            return 0;
+//        } else if ((3 <= x) && (x <= 5)) {
+//            return 60;
+//        } else {
+//            return 20;
+//        }
+//    }
+//
+//    private int determineY(int y) {
+//        if ((0 <= y) && (y <= 2)) {
+//            return 0;
+//        } else if ((3 <= y) && (y <= 5)) {
+//            return 25;
+//        } else {
+//            return 10;
+//        }
+//    }
 
     private boolean matrixCheck(int x, int y, int digit) {
         int a = y + 2;
@@ -76,33 +77,57 @@ public class check extends verification {
                     return true;
                 }
             }
-            y = 0;
+            y = a-2;
         }
         return false;
     }
 
     private boolean checkQuadrant(int x, int y, int digit) {
-        switch (determineY(y) + determineX(x)) {
-            case (0):
-                return matrixCheck(0, 0, digit);
-            case (60):
-                return matrixCheck(3, 0, digit);
-            case (20):
-                return matrixCheck(6, 0, digit);
-            case (25):
-                return matrixCheck(0, 3, digit);
-            case (85):
-                return matrixCheck(3, 3, digit);
-            case (45):
-                return matrixCheck(6, 3, digit);
-            case (10):
-                return matrixCheck(0, 6, digit);
-            case (70):
-                return matrixCheck(3, 6, digit);
-            case (30):
-                return matrixCheck(6, 6, digit);
-            default:
-                return true;
+        if (((0 <= x) && (x <= 2)) && ((0 <= y) && (y <= 2))) {
+            return matrixCheck(0, 0, digit);
+        } else if (((0 <= x) && (x <= 2)) && ((3 <= y) && (y <= 5))) {
+            return matrixCheck(3, 0, digit);
+        } else if (((0 <= x) && (x <= 2)) && ((6 <= y) && (y <= 8))) {
+            return matrixCheck(6, 0, digit);
+        } else if (((3 <= x) && (x <= 5)) && ((0 <= y) && (y <= 2))) {
+            return matrixCheck(0, 3, digit);
+        } else if (((3 <= x) && (x <= 5)) && ((3 <= y) && (y <= 5))) {
+            return matrixCheck(3, 3, digit);
+        } else if (((3 <= x) && (x <= 5)) && ((5 <= y) && (y <= 8))) {
+            return matrixCheck(6, 3, digit);
+        } else if (((6 <= x) && (x <= 8)) && ((0 <= y) && (y <= 2))) {
+            return matrixCheck(0, 6, digit);
+        } else if (((6 <= x) && (x <= 8)) && ((3 <= y) && (y <= 5))) {
+            return matrixCheck(3, 6, digit);
+        } else if (((6 <= x) && (x <= 8)) && ((5 <= y) && (y <= 8))) {
+            return matrixCheck(6, 6, digit);
+        } else {
+            return false;
         }
     }
+    
+//    private boolean checkQuadrant(int x, int y, int digit) {
+//        switch (determineY(y) + determineX(x)) {
+//            case (0):
+//                return matrixCheck(0, 0, digit);
+//            case (60):
+//                return matrixCheck(3, 0, digit);
+//            case (20):
+//                return matrixCheck(6, 0, digit);
+//            case (25):
+//                return matrixCheck(0, 3, digit);
+//            case (85):
+//                return matrixCheck(3, 3, digit);
+//            case (45):
+//                return matrixCheck(6, 3, digit);
+//            case (10):
+//                return matrixCheck(0, 6, digit);
+//            case (70):
+//                return matrixCheck(3, 6, digit);
+//            case (30):
+//                return matrixCheck(6, 6, digit);
+//            default:
+//                return true;
+//        }
+//    }
 }
