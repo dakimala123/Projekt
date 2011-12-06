1da894acaa418e2aa2c9063f1a01de95747a44d8
Farshid Besharati <farshidbt@gmail.com>
2011-12-06 14:32:50 +0100

    VerifyMatrix

--- solve/check.java
+++ solve/check.java
@@ -21,66 +21,6 @@ public class check {
         return false;
     }
 
-    public boolean verify() {
-        if (verifyVert() && verifyHor() && verifyMat()) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean verifyVert() {
-        int x = 0;
-        int y = 0;
-        int i = 1;
-
-        for (; x < 9; x++) {
-            while (y < 9) {
-                if (i > 9) {
-                    i = 1;
-                }
-
-                if (sArray[y][x] == i) {
-                    y++;
-                } else {
-                    i++;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private boolean verifyHor() {
-        int x = 0;
-        int y = 0;
-        int i = 1;
-
-        for (; y < 9; y++) {
-            while (x < 9) {
-                if (i > 9) {
-                    i = 1;
-                }
-
-                if (sArray[y][x] == i) {
-                    x++;
-                } else {
-                    i++;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private boolean verifyMat() {
-        int x = 0;
-        int y = 0;
-        int i = 1;
-
-        
-
-    }
-
     private boolean checkHor(int coord, int digit) {
         int i;
 
@@ -159,5 +99,66 @@ public class check {
             default:
                 return true;
         }
+    }
+
+    
+    public boolean verify() {
+        if (verifyVert() && verifyHor() && verifyMat()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean verifyVert() {
+        int x = 0;
+        int y = 0;
+        int i = 1;
+
+        for (; x < 9; x++) {
+            while (y < 9) {
+                if (i > 9) {
+                    i = 1;
+                }
+
+                if (sArray[y][x] == i) {
+                    y++;
+                } else {
+                    i++;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean verifyHor() {
+        int x = 0;
+        int y = 0;
+        int i = 1;
+
+        for (; y < 9; y++) {
+            while (x < 9) {
+                if (i > 9) {
+                    i = 1;
+                }
+
+                if (sArray[y][x] == i) {
+                    x++;
+                } else {
+                    i++;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean verifyMat() {
+        int x = 0;
+        int y = 0;
+        int i = 1;
+
+        
+
     }
 }
