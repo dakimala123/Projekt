0e8205efc1242b4b9634dbf0354983ae0c08fd90
Farshid Besharati <farshidbt@gmail.com>
2011-12-06 17:19:31 +0100

    PUSHAR BARA FÖR ATT IRRITERA LOVE!

--- solve/check.java
+++ solve/check.java
@@ -47,26 +47,6 @@ public class check extends verification {
         }
         return false;
     }
-//
-//    private int determineX(int x) {
-//        if ((0 <= x) && (x <= 2)) {
-//            return 0;
-//        } else if ((3 <= x) && (x <= 5)) {
-//            return 60;
-//        } else {
-//            return 20;
-//        }
-//    }
-//
-//    private int determineY(int y) {
-//        if ((0 <= y) && (y <= 2)) {
-//            return 0;
-//        } else if ((3 <= y) && (y <= 5)) {
-//            return 25;
-//        } else {
-//            return 10;
-//        }
-//    }
 
     private boolean matrixCheck(int x, int y, int digit) {
         int a = y + 2;
@@ -105,29 +85,4 @@ public class check extends verification {
             return false;
         }
     }
-    
-//    private boolean checkQuadrant(int x, int y, int digit) {
-//        switch (determineY(y) + determineX(x)) {
-//            case (0):
-//                return matrixCheck(0, 0, digit);
-//            case (60):
-//                return matrixCheck(3, 0, digit);
-//            case (20):
-//                return matrixCheck(6, 0, digit);
-//            case (25):
-//                return matrixCheck(0, 3, digit);
-//            case (85):
-//                return matrixCheck(3, 3, digit);
-//            case (45):
-//                return matrixCheck(6, 3, digit);
-//            case (10):
-//                return matrixCheck(0, 6, digit);
-//            case (70):
-//                return matrixCheck(3, 6, digit);
-//            case (30):
-//                return matrixCheck(6, 6, digit);
-//            default:
-//                return true;
-//        }
-//    }
 }
